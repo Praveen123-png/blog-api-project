a041b7538d88593b7a60373bd3b9675759cd7d15
Praveen123-png <praveenkumar814444@gmail.com>
2025-07-03 15:43:15 +0530

    time change

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let posts = [
         content:
             "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
         author: "Alex Thompson",
-        date: "02/07/2025, 3:30:11 pm",
+        date: "01/07/2025, 3:30:11 pm",
     },
     {
         id: 2,
@@ -28,7 +28,7 @@ let posts = [
         content:
             "Sustainability is more than just a buzzword; it's a way of life. As the effects of climate change become more pronounced, there's a growing realization about the need to live sustainably. From reducing waste and conserving energy to supporting eco-friendly products, there are numerous ways we can make our daily lives more environmentally friendly. This post will explore practical tips and habits that can make a significant difference.",
         author: "Samuel Green",
-        date: "02/07/2025, 06:33:23 am",
+        date: "02/07/2025, 06:33:23 pm",
     },
 ];
 
